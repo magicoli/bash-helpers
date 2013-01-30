4449e596715de9bbc233d3a0dbf22e34f5102e3b
Olivier van Helden (beegie) <olivier@van-helden.net>
2013-01-30 16:27:45 -0400

    Clean up. Moved useless files to deprecated/ or archives/ folders, accordingly

--- olddns/hostingcheck.sh
+++
@@ -1,8 +0,0 @@
-LIST=`cat $@`
-for SITE in $LIST
-do 
-#	echo -n $SITE -
-	A=$(host $SITE | grep "has address" | cut -d " " -f 4)
-#	echo -n www.$SITE
-	B=$(host www.$SITE | grep "has address" | cut -d " " -f 4)
-done
